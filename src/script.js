41530a0e92aa9b7f1633f784dc669d93e9e26c18
David Moreno <moreno@rushstreetinteractive.com>
2024-05-05 16:49:49 -0500

    Adding animations

---
+++ src/script.js
@@ -0,0 +1,65 @@
+import * as THREE from 'three'
+import gsap from 'gsap'
+
+// Canvas
+const canvas = document.querySelector('canvas.webgl')
+
+// Scene
+const scene = new THREE.Scene()
+
+// Object
+const geometry = new THREE.BoxGeometry(1, 1, 1)
+const material = new THREE.MeshBasicMaterial({ color: 0xff0000 })
+const mesh = new THREE.Mesh(geometry, material)
+scene.add(mesh)
+
+// Sizes
+const sizes = {
+    width: 800,
+    height: 600
+}
+
+// Camera
+const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height)
+camera.position.z = 3
+scene.add(camera)
+
+// Renderer
+const renderer = new THREE.WebGLRenderer({
+    canvas: canvas
+})
+renderer.setSize(sizes.width, sizes.height)
+
+// Time
+// let time = Date.now()
+
+// Better solution with clock instead of Date.now()
+// const clock = new THREE.Clock()
+
+gsap.to(mesh.position, { duration: 1, delay: 1, x: 2 })
+gsap.to(mesh.position, { duration: 1, delay: 2, x: 0 })
+
+// Animations
+const tick = () => {
+    /* const currentTime = Date.now()
+    const deltaTime = currentTime - time
+    time = currentTime */
+    // const elapsedTime = clock.getElapsedTime()
+
+    // update Objects
+    // mesh.position.x += 0.01.
+    // speed of animation depends on FPS capacity of each computer
+    // that's why we need deltaTime, to guarantee same speed everywhere
+    // mesh.rotation.y += 0.001 * deltaTime
+
+    //mesh.rotation.y = elapsedTime
+    // one rotation per second
+    // mesh.rotation.y = elapsedTime * Math.PI * 2
+    // mesh.position.y = Math.sin(elapsedTime)
+
+    // Render
+    renderer.render(scene, camera)
+
+    window.requestAnimationFrame(tick)
+}
+tick()
